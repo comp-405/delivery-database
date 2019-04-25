871a0de84faad1eadebdfe889d9ac8031838089f
Giulian Santos <G1santos@student.bridgew.edu>
2019-04-25 13:38:29 -0400

    Added models to models.py

--- delivery_database/delivery/models.py
+++ delivery_database/delivery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class driver(models.Model):
+    user_id = models.AutoField(prinmary_key=true)
+    SSN = models.Charfield(max_length=10)
+    name = models.Charfield(max_length = 50)
+    email = models.Charfield(max_length = 80)
+    state = models.Charfield(max_length = 80)
+    city = models.Charfield(max_length = 80)
+    Company = models.Charfield(max_length = 80)
+
+class vehicles(models.Model):
+    models = models.charfield(max_length = 60)
+    colors = models.Charfield(max_length = 60)
+    year = models.Charfield(max_length = 60)
+
+class delivery(model.Model):
+    amount = models.interfield()
+    tip = models.interfield()
+    driver = models.ForeignKey(driver.related_name = 'delivery', on_delete = models.CASCADE)
+    vehicle = models.ForeignKey(vehicle.related_name = 'delivery', on_delete = models.CASCADE)
