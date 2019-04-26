63a4fd76138bc74bb2fb0aba68f2cf02a2fa0c40
cberry216 <christopherberry216@gmail.com>
2019-04-26 09:55:14 -0400

    Added urls to delivery app.

---
+++ delivery_database/delivery/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import (
+  home,
+  DriverCreateView,
+  VehicleCreateView,
+  select_driver,
+  select_vehicle,
+  create_delivery
+)
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('new-delivery/', select_driver, name='select_driver'),
+    path('new-delivery/<int:driver>/', select_vehicle, name='select_vehicle'),
+    path('new-delivery/<int:driver>/<int:vehicle>/', create_delivery, name='create_delivery'),
+]
