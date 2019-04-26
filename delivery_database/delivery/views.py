6bfe8a391de56400ceb768bc6fd45b98fcee31ef
cberry216 <christopherberry216@gmail.com>
2019-04-26 08:48:06 -0400

    created views.

--- delivery_database/delivery/views.py
+++ delivery_database/delivery/views.py
@@ -8,3 +8,17 @@ class DriverCreateView(CreateView):
     model = Driver
     template_name = 'driver_new.html'
     fields = '__all__'
+
+class VehicleCreateView(CreateView):
+    model = Vehicle
+    template_name = 'vehicle_new.html'
+    fields = '__all__'
+
+def select_user(request):
+    pass
+
+def select_vehicle(request, driver):
+    pass
+
+def create_delivery(request, driver, vehicle):
+    pass
