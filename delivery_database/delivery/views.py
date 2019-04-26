31c1f7522ad81ca041003ba052e282cb5aab40e3
cberry216 <christopherberry216@gmail.com>
2019-04-26 08:36:20 -0400

    Created driver create view.

--- delivery_database/delivery/views.py
+++ delivery_database/delivery/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+
+from .models import Driver, Vehicle, Delivery
 
 # Create your views here.
+
+class DriverCreateView(CreateView):
+    model = Driver
+    template_name = 'driver_new.html'
+    fields = '__all__'
