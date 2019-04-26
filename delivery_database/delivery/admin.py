7bb2951a66b46e9e73a172418a962873276a3e44
cberry216 <christopherberry216@gmail.com>
2019-04-26 08:14:42 -0400

    Registered all models in admin.

--- delivery_database/delivery/admin.py
+++ delivery_database/delivery/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Driver, Vehicle, Delivery
+
 # Register your models here.
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'ssn', 'name', 'state', 'company']
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['model', 'color', 'year']
+
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'tip', 'driver', 'vehicle']
