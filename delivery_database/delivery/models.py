9e861edba621702414d68b9fdb12f2218fa97e0a
cberry216 <christopherberry216@gmail.com>
2019-04-26 12:43:14 -0400

    Added choices to driver.state.

--- delivery_database/delivery/models.py
+++ delivery_database/delivery/models.py
@@ -3,11 +3,62 @@ from django.db import models
 # Create your models here.
 
 class Driver(models.Model):
+    STATE_CHOICES = [
+      ('AL', 'Alabama'),
+      ('AK', 'Alaska'),
+      ('AZ', 'Arizona'),
+      ('AR', 'Arkansa'),
+      ('CA', 'California'),
+      ('CO', 'Colorado'),
+      ('CT', 'Connecticut'),
+      ('DE', 'Delaware'),
+      ('FL', 'Florida'),
+      ('GA', 'Georgia'),
+      ('HI', 'Hawaii'),
+      ('ID', 'Idaho'),
+      ('IL', 'Illinois'),
+      ('IN', 'Indiana'),
+      ('KS', 'Kansas'),
+      ('KY', 'Kentucky'),
+      ('LA', 'Lousianna'),
+      ('ME', 'Maine'),
+      ('MD', 'Maryland'),
+      ('MA', 'Massachusetts'),
+      ('MI', 'Michigan'),
+      ('MN', 'Minnesota'),
+      ('MS', 'Mississippi'),
+      ('MO', 'Missouri'),
+      ('MT', 'Montana'),
+      ('NE', 'Nebraska'),
+      ('NV', 'Nevada'),
+      ('NH', 'New Hampshire'),
+      ('NJ', 'New Jersey'),
+      ('NM', 'New Mexico'),
+      ('NY', 'New York'),
+      ('NC', 'North Carolina'),
+      ('ND', 'North Dakota'),
+      ('OH', 'Ohio'),
+      ('OR', 'Oregon'),
+      ('PA', 'Pennsylvania'),
+      ('RI', 'Rhode Island'),
+      ('SC', 'South Carolina'),
+      ('SD', 'South Dakota'),
+      ('TN', 'Tennessee'),
+      ('TX', 'Texas'),
+      ('UT', 'Utah'),
+      ('VT', 'Vermont'),
+      ('VA', 'Virginia'),
+      ('WA', 'Washington'),
+      ('WV', 'West Virginia'),
+      ('WI', 'Wisconsin'),
+      ('WY', 'Wyoming')
+    ]
+
     user_id = models.AutoField(primary_key=True)
     ssn = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=80)
-    state = models.CharField(max_length=80)
+    state = models.CharField(max_length=80, choices=STATE_CHOICES)
     city = models.CharField(max_length=80)
     company = models.CharField(max_length=80)
 
