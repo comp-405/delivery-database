48ffafb530c7ce790b43b8c7bd208f16b42c86d4
cberry216 <christopherberry216@gmail.com>
2019-04-26 12:43:27 -0400

    Added URLConf for create views.

--- delivery_database/delivery/urls.py
+++ delivery_database/delivery/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('new-delivery/', select_driver, name='select_driver'),
     path('new-delivery/<int:driver>/', select_vehicle, name='select_vehicle'),
     path('new-delivery/<int:driver>/<int:vehicle>/', create_delivery, name='create_delivery'),
+    path('new-driver/', DriverCreateView.as_view(), name='new_driver'),
+    path('new-vehicle/', VehicleCreateView.as_view(), name='new_vehicle'),
 ]
