2627d92a145a34bc7991a95c89a1342e78684cd8
cberry216 <christopherberry216@gmail.com>
2019-04-29 17:29:52 -0400

    Finished division tag.

--- delivery_database/delivery/templatetags/delivery_tags.py
+++ delivery_database/delivery/templatetags/delivery_tags.py
@@ -5,6 +5,6 @@ register = template.Library()
 @register.simple_tag
 def divide(n, d):
   try:
-    return float(n) / float(d)
+    return "{:.2f}".format((int(n)/int(d)) * 100)
   except (ValueError, ZeroDivisionError):
     return None
