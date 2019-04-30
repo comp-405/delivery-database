2f6555e84ca9c8b8f5dbc4f2fc8692739633b19c
cberry216 <christopherberry216@gmail.com>
2019-04-30 13:14:09 -0400

    Reverted staticfiles changes.

--- delivery_database/delivery_database/settings.py
+++ delivery_database/delivery_database/settings.py
@@ -50,10 +50,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-MIDDLEWARE_CLASSES = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
-
 ROOT_URLCONF = 'delivery_database.urls'
 
 TEMPLATES = [
@@ -123,6 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
